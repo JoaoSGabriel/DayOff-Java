8d16d6c8bd7307629bcb006f79d5fbfa372316f7
JoaoSGabriel <joaogabriel0359@gmail.com>
2023-03-25 13:46:27 -0300

    feat: create holidays array

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Holiday[] holidays = new Holiday[]{
+            new Holiday("01/01/2023", "Confraternização mundial"),
+            new Holiday("21/02/2023", "Carnaval"),
+            new Holiday("17/04/2023", "Páscoa"),
+            new Holiday("21/04/2023", "Tiradentes"),
+            new Holiday("01/05/2023", "Dia do trabalho"),
+            new Holiday("08/06/2023", "Corpus Christi"),
+            new Holiday("07/09/2023", "Independência do Brasil"),
+            new Holiday("12/10/2023", "Nossa Senhora Aparecida"),
+            new Holiday("02/11/2023", "Finados"),
+            new Holiday("15/11/2023", "Proclamação da República"),
+            new Holiday("25/12/2023", "Natal")
+        };
+
+        HolyDayzer function = new HolyDayzer(holidays);
     }
 }
---
+++ src/HolyDayzer.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HolyDayzer {
+    private Holiday[] holidays;
+
+    HolyDayzer(Holiday[] data) {
+        this.holidays = data;
+    }
+
+    
+}
